3a6d8d99d5ab65cf13387e917031df531e1fceda
Jabbar <jabbarkhan31416@gmail.com>
2019-10-21 17:02:31 +0530

    0

---
+++ w3/clock/clock.js
@@ -0,0 +1,37 @@
+
+
+window.addEventListener("DOMContentLoaded", e=>{
+    const canvas = document.getElementsByTagName("canvas")[0]
+    const ctx = canvas.getContext("2d")
+    const resizeDraw = ()=>{
+        const width = window.innerWidth
+        const height = window.innerHeight
+        const size = Math.min(width,height)
+        canvas.setAttribute("width",size)
+        canvas.setAttribute("height",size)
+        const date = new Date()
+        const hour = date.getHours()%12*5
+        const minute = date.getMinutes()
+        const second = date.getSeconds()
+        const drawHand = (angle,length)=>{
+            length1 = length*size/2*0.8
+            angle1 = Math.floor(angle)*Math.PI/30
+            ctx.lineWidth = size/100
+            ctx.lineCap = "round"
+            ctx.beginPath()
+            ctx.moveTo(size/2, size/2)
+            ctx.lineTo(
+                size/2 + length1*Math.sin(angle1),
+                size/2 - length1*Math.cos(angle1)
+            )
+            ctx.stroke()
+        }
+        drawHand(hour, 0.5)
+        drawHand(minute, 0.8)
+        drawHand(second, 1)
+        ctx.beginPath()
+        ctx.arc(size/2, size/2, size/36, 0, 2*Math.PI)
+        ctx.fill()
+    }
+    window.setInterval(resizeDraw,1000)
+})
